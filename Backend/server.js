6f93adb15919bb5d897f1b83555c6ebb210b4af0
hryshkanych <105241875+luff3@users.noreply.github.com>
2024-05-14 19:21:05 +0300

    done authControlers with jwt and messageconttrollers, models for chat

--- Backend/server.js
+++ Backend/server.js
@@ -3,15 +3,26 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./Routes/authRoute.js"
+import messageRoutes from "./Routes/messageRoutes.js"
+import userRoutes from "./Routes/userRoute.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
 dotenv.config();
 
-
 const PORT = process.env.PORT || 5000;
-const app = express();
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
-app.use('/api/auth', authRoutes)
+const app = express();
+
+app.use(express.json()); 
+
+app.use(cookieParser())
+
+app.use('/api/auth', authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
 app.listen(PORT, () => {
     connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
