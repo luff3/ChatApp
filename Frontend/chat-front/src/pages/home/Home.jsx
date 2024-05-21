4a3094812fe9ace9957376708cacda49a69199b4
hryshkanych <105241875+luff3@users.noreply.github.com>
2024-05-21 17:29:48 +0300

    add ui for app

---
+++ Frontend/chat-front/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import MessageContainer from '../../components/messages/MessageContainer'
+import Login from '../login/Login';
+import SignUp from '../signup/SignUp';
+
+const Home = () => {
+	return (
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			{/* <Sidebar />
+			<MessageContainer /> */}
+            <SignUp />
+		</div>
+	);
+};
+export default Home;
